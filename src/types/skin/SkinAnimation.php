5c21bb1de870ee6e4125eabbfa6f17ecb3e3250b
xRookieFight <me@xrookiefight.com>
2026-08-19 23:10:50 +0300

    Forgot this one too

--- src/types/skin/SkinAnimation.php
+++ src/types/skin/SkinAnimation.php
@@ -27,9 +27,10 @@ namespace pocketmine\network\mcpe\protocol\types\skin;
 
 class SkinAnimation{
 
-	public const TYPE_HEAD = 1;
+	public const TYPE_NONE = 0;
+	public const TYPE_FACE = 1;
 	public const TYPE_BODY_32 = 2;
-	public const TYPE_BODY_64 = 3;
+	public const TYPE_BODY_128 = 3;
 
 	public const EXPRESSION_LINEAR = 0; //???
 	public const EXPRESSION_BLINKING = 1;
