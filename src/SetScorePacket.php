8c5d321e0b0a74a6374edb4d0ae55697025e3258
xRookieFight <me@xrookiefight.com>
2026-08-20 17:54:54 +0300

    Bedrock 1.26.45 (#17)
    * Bedrock 1.26.45
    
    * Forgot this
    
    * Forgot this one too
    
    * Revert this one

--- src/SetScorePacket.php
+++ src/SetScorePacket.php
@@ -70,7 +70,7 @@ class SetScorePacket extends DataPacket implements ClientboundPacket{
 			switch($entry->type){
 				case ScorePacketEntry::TYPE_REMOVE:
 					$entry->scoreboardId = VarInt::readSignedLong($in);
-					$entry->objectiveName = CommonTypes::readDoubleOptional($in, CommonTypes::getString(...)); //BLAMEMOJANG: THIS IS PURE RAGEBAIT. WHAT THE FUCK DOES THIS EVEN MEAN
+					$entry->objectiveName = CommonTypes::readOptional($in, CommonTypes::getString(...));
 					break;
 				case ScorePacketEntry::TYPE_PLAYER:
 				case ScorePacketEntry::TYPE_ENTITY:
@@ -101,7 +101,7 @@ class SetScorePacket extends DataPacket implements ClientboundPacket{
 			switch($entry->type){
 				case ScorePacketEntry::TYPE_REMOVE:
 					VarInt::writeSignedLong($out, $entry->scoreboardId);
-					CommonTypes::writeDoubleOptional($out, $entry->objectiveName, CommonTypes::putString(...));
+					CommonTypes::writeOptional($out, $entry->objectiveName, CommonTypes::putString(...));
 					break;
 				case ScorePacketEntry::TYPE_PLAYER:
 				case ScorePacketEntry::TYPE_ENTITY:
